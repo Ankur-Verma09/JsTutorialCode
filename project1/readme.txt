916ced4f3d91aad928daa436d58e6a5c13e87658
ankur-verma09 <er.ankurverma09@gmail.com>
2024-05-21 15:27:14 +0530

    style added

--- project1/readme.txt
+++ project1/readme.txt
@@ -1,6 +1,13 @@
 Smoothing Scrolling
-    Attach loco scroll css
-    Attach locomotive scroll in js
-    some code from loco github for js
+    Attach loco scroll css --> https://cdn.jsdelivr.net/npm/locomotive-scroll@3.5.4/dist/
+    Attach locomotive scroll in js 
+    some code from loco github for js --> https://github.com/locomotivemtl/locomotive-scroll?tab=readme-ov-file
 gsap
+    Add gsap script --> https://cdnjs.com/libraries/gsap
+    
 ScrollingTrigger
+
+
+thorrtel --> consistency
+debounce --> hold till the previous action gets completed
+impletent scroll behaivour function instead of locomotive librarie
--- project1/script.js
+++ project1/script.js
@@ -1,6 +1,30 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.form("#nav", {
+        y:'-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut,
+    })
+    .to(".boundingelem", {
+        y: 0,
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut,
+        stagger: .2
+    })
+}
+
+function circleMouseFollower(){
+    window.addEventListener("mousemove", function(dets){
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    })
+}
+
+circleMouseFollower();
+firstPageAnim();
